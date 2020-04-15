d5bf6ffbed2cd3202b74f53d41e2705ee8f7722c
Chris Simpkins <git.simpkins@gmail.com>
2020-04-15 17:51:16 -0400

    [ops::path] add path_has_extension function
    used for filtering file paths by extension

--- src/ops/path.rs
+++ src/ops/path.rs
@@ -17,6 +17,29 @@ where
     }
 }
 
+/// Returns a boolean that indicates whether the `filepath` parameter
+/// includes the `extension` file extension.  Also returns `false` in
+/// cases where the filepath does not exist or there is no extension.
+pub(crate) fn path_has_extension<P>(filepath: P, extension: &str) -> bool
+where
+    P: Into<PathBuf>,
+{
+    match filepath.into().extension() {
+        Some(ext) => {
+            // permissive comparison of the extension
+            // allows for use of period character in
+            // the parameter (e.g., `.txt`)
+            if extension.starts_with(".") {
+                return ext.to_string_lossy() == &extension[1..];
+            }
+            // or no period character in the extension
+            // parameter (e.g., `txt`)
+            return ext.to_string_lossy() == extension;
+        }
+        None => return false,
+    }
+}
+
 /// Returns a boolean that indicates whether there is a dot directory
 /// or dot file anywhere in the canonical absolute path to the file.
 /// This is used as a filter during execution and requires a valid
@@ -42,6 +65,9 @@ where
 mod tests {
     use super::*;
 
+    // ======================================
+    // get_absolute_filepath function tests
+    // ======================================
     #[test]
     fn test_get_absolute_filepath_good_path() {
         let testpath = get_absolute_filepath("./Cargo.toml");
@@ -55,6 +81,33 @@ mod tests {
         assert!(testpath.is_err());
     }
 
+    // ======================================
+    // path_has_extension function tests
+    // ======================================
+    #[test]
+    fn test_path_has_extension_with_correct_extension() {
+        let testpath = Path::new("./tests/testfiles/path/test.txt");
+        assert!(path_has_extension(testpath, ".txt"));
+        assert!(path_has_extension(testpath, "txt"));
+    }
+
+    #[test]
+    fn test_path_has_extension_with_incorrect_extension() {
+        let testpath = Path::new("./tests/testfiles/path/test.txt");
+        assert_eq!(path_has_extension(testpath, ".yaml"), false);
+        assert_eq!(path_has_extension(testpath, "yaml"), false);
+    }
+
+    #[test]
+    fn test_path_has_extension_with_no_extension() {
+        let testpath = Path::new("./tests/testfiles/path/testfile");
+        assert_eq!(path_has_extension(testpath, ".txt"), false);
+        assert_eq!(path_has_extension(testpath, "txt"), false);
+    }
+
+    // ======================================
+    // path_is_hidden function tests
+    // ======================================
     #[test]
     fn test_path_is_hidden_with_dotfile() {
         let testpath = Path::new("./tests/testfiles/path/.testfile");
@@ -62,8 +115,14 @@ mod tests {
     }
 
     #[test]
-    fn test_path_is_hidden_with_dotdir() {
+    fn test_path_is_hidden_with_dotdir_in_path() {
         let testpath = Path::new("./tests/testfiles/.dotdir/testfile");
+        assert!(path_is_hidden(testpath));
+    }
+
+    #[test]
+    fn test_path_is_hidden_with_dotdir_only() {
+        let testpath = Path::new("./tests/testfiles/.dotdir");
         assert!(path_is_hidden(testpath));
     }
 
