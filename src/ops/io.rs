9741b079f9962472f8d22866fd3051a30ea03e00
Chris Simpkins <git.simpkins@gmail.com>
2020-04-17 10:06:22 -0400

    [ops::io] fix tests in io module so that they support cross platform testing

--- src/ops/io.rs
+++ src/ops/io.rs
@@ -20,22 +20,20 @@ mod tests {
     fn test_walk_with_dir() {
         let mut dirpaths = walk("./tests/testfiles/io/stablepaths");
         let dirpaths_len_check = walk("./tests/testfiles/io/stablepaths");
-        assert_eq!(
-            dirpaths.next().unwrap().unwrap().path(),
-            Path::new("./tests/testfiles/io/stablepaths")
-        );
-        assert_eq!(
-            dirpaths.next().unwrap().unwrap().path(),
-            Path::new("./tests/testfiles/io/stablepaths/test")
-        );
-        assert_eq!(
-            dirpaths.next().unwrap().unwrap().path(),
-            Path::new("./tests/testfiles/io/stablepaths/README.md")
-        );
-        assert_eq!(
-            dirpaths.next().unwrap().unwrap().path(),
-            Path::new("./tests/testfiles/io/stablepaths/test.txt")
-        );
+        let expected_list = [
+            Path::new("./tests/testfiles/io/stablepaths"),
+            Path::new("./tests/testfiles/io/stablepaths/test"),
+            Path::new("./tests/testfiles/io/stablepaths/README.md"),
+            Path::new("./tests/testfiles/io/stablepaths/test.txt"),
+        ];
+        // run through all expected files in iterator and check against
+        // contents of expected list.
+        // Cannot test order directly because order differs across
+        // macOS, Win, GNU/Linux platforms based on CI testing results
+        assert!(expected_list.contains(&dirpaths.next().unwrap().unwrap().path()));
+        assert!(expected_list.contains(&dirpaths.next().unwrap().unwrap().path()));
+        assert!(expected_list.contains(&dirpaths.next().unwrap().unwrap().path()));
+        assert!(expected_list.contains(&dirpaths.next().unwrap().unwrap().path()));
 
         let mut index = 0;
         for _ in dirpaths_len_check {
@@ -63,6 +61,11 @@ mod tests {
 
     #[test]
     fn test_walk_with_filter_map() {
+        let expected_list = [
+            PathBuf::from("./tests/testfiles/io/stablepaths/test"),
+            PathBuf::from("./tests/testfiles/io/stablepaths/README.md"),
+            PathBuf::from("./tests/testfiles/io/stablepaths/test.txt"),
+        ];
         // filter_map to filter out directories that process does not have permission
         // to access
         let mut file_entries = walk("./tests/testfiles/io/stablepaths")
@@ -74,18 +77,9 @@ mod tests {
                     None
                 }
             });
-        assert_eq!(
-            file_entries.next(),
-            Some(PathBuf::from("./tests/testfiles/io/stablepaths/test"))
-        );
-        assert_eq!(
-            file_entries.next(),
-            Some(PathBuf::from("./tests/testfiles/io/stablepaths/README.md"))
-        );
-        assert_eq!(
-            file_entries.next(),
-            Some(PathBuf::from("./tests/testfiles/io/stablepaths/test.txt"))
-        );
-        assert!(file_entries.next().is_none()); // the above should have exhausted the file paths in the test
+        assert!(expected_list.contains(&file_entries.next().unwrap()));
+        assert!(expected_list.contains(&file_entries.next().unwrap()));
+        assert!(expected_list.contains(&file_entries.next().unwrap()));
+        assert_eq!(file_entries.next(), None); // the above should have exhausted the file paths in the test
     }
 }
