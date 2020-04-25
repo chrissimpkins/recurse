6c5d08fc0deefe07ad0b7dd01223d999e19550ae
Chris Simpkins <git.simpkins@gmail.com>
2020-04-25 05:36:07 -0400

    [command::walk] refactor filtered directory walk to new method
    Exposes the method for use throughout the rest of the crate so that we can use a uniform dir walk approach without code duplication

--- src/command/walk.rs
+++ src/command/walk.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use anyhow::{anyhow, Result};
 
 use crate::command::Command;
@@ -18,39 +20,60 @@ impl Command for WalkCommand {
             symlinks,
         } = subcmd
         {
-            let has_extension_filter = extension.is_some();
-            let file_entries = walk(inpath, mindepth, maxdepth, symlinks)
-                .filter_map(|f| f.ok()) // filter paths that process does not have permission to edit
-                .filter_map(|f| {
-                    // filter paths on *files only* (i.e., eliminate dir paths, we don't need them)
-                    if f.path().is_file() {
-                        Some(f.path().to_owned())
-                    } else {
-                        None
-                    }
-                });
-
             let mut stdout_string = String::new();
-            for filepath in file_entries {
-                // =================
-                // File path filters
-                // =================
-                if !hidden && path_is_hidden(&filepath) {
-                    // if file is in a hidden path, skip it
-                    continue;
-                } else if has_extension_filter {
-                    // if user requested extension filter, filter on it
-                    if path_has_extension(&filepath, extension.as_ref().unwrap()) {
-                        stdout_string.push_str(&filepath.to_string_lossy());
-                        stdout_string.push_str("\n");
-                    }
-                } else {
-                    stdout_string.push_str(&filepath.to_string_lossy());
-                    stdout_string.push_str("\n");
-                }
+            for path in
+                WalkCommand::walk_path(extension, hidden, mindepth, maxdepth, symlinks, inpath)
+            {
+                stdout_string.push_str(&path.to_string_lossy());
+                stdout_string.push_str("\n");
             }
-            return Ok(stdout_string);
+            Ok(stdout_string)
+        } else {
+            Err(anyhow!("failure to parse walk subcommand."))
         }
-        return Err(anyhow!("failure to parse walk subcommand."));
+    }
+}
+
+impl WalkCommand {
+    /// Walks an `inpath` for a filtered Vec<PathBuf> based on the parameter
+    /// definitions.
+    pub(crate) fn walk_path(
+        extension: Option<String>,
+        hidden: bool,
+        mindepth: Option<usize>,
+        maxdepth: Option<usize>,
+        symlinks: bool,
+        inpath: PathBuf,
+    ) -> Vec<PathBuf> {
+        let has_extension_filter = extension.is_some();
+        let file_entries = walk(inpath, mindepth, maxdepth, symlinks)
+            .filter_map(|f| f.ok()) // filter paths that process does not have permission to edit
+            .filter_map(|f| {
+                // filter paths on *files only* (i.e., eliminate dir paths, we don't need them)
+                if f.path().is_file() {
+                    Some(f.path().to_owned())
+                } else {
+                    None
+                }
+            });
+
+        let mut path_vec: Vec<PathBuf> = Vec::new();
+        for filepath in file_entries {
+            // =================
+            // File path filters
+            // =================
+            if !hidden && path_is_hidden(&filepath) {
+                // if file is in a hidden path, skip it
+                continue;
+            } else if has_extension_filter {
+                // if user requested extension filter, filter on it
+                if path_has_extension(&filepath, extension.as_ref().unwrap()) {
+                    path_vec.push(filepath);
+                }
+            } else {
+                path_vec.push(filepath);
+            }
+        }
+        path_vec
     }
 }
