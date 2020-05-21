7cea8f927dd7a0ad0b9936d699c5244760379ca0
Chris Simpkins <git.simpkins@gmail.com>
2020-05-20 22:41:02 -0400

    [command::replace] initial implementation of replace subcommand

--- src/command/replace.rs
+++ src/command/replace.rs
@@ -1,5 +1,6 @@
-use std::fs::{read_to_string, write};
-use std::io::ErrorKind;
+use std::fs::{read_to_string, write, OpenOptions};
+use std::io::prelude::*;
+use std::io::{BufWriter, ErrorKind};
 use std::path::{Path, PathBuf};
 
 use anyhow::{anyhow, Result};
@@ -9,6 +10,8 @@ use crate::command::Command;
 use crate::ops::io::walk;
 use crate::ops::path::{path_has_extension, path_is_hidden};
 use crate::Shot;
+
+const SECONDARY_FILEPATH_EXTENSION: &str = "2";
 
 pub(crate) struct ReplaceCommand {}
 
@@ -26,6 +29,8 @@ impl Command for ReplaceCommand {
             replace,
         } = subcmd
         {
+            // Protect against accidental attempts to replace every path beginning at root
+            // when a path typo of `/` (Unix) or `\` (Win) is used on the command line
             if is_root_filepath(&inpath) {
                 return Err(anyhow!(
                     "shot does not support replacements originating on the file path '{}'",
@@ -37,7 +42,16 @@ impl Command for ReplaceCommand {
             for entry in walk(inpath, &mindepth, &maxdepth, &symlinks).filter_map(|f| f.ok()) {
                 if entry.metadata().unwrap().is_file() {
                     let filepath = entry.path();
-                    if !hidden && path_is_hidden(filepath) {
+                    if has_secondary_extension(&filepath) {
+                        // If file has the secondary extension that is used by
+                        // this application, do not perform string replacement
+                        // in that file. Use the original and overwrite the
+                        // secondary file with previous replacements.
+                        // Removal of this check will lead to additional secondary
+                        // files with new extensions based on the last secondary
+                        // file path in sequence.  This is not desired behavior.
+                        continue;
+                    } else if !hidden && path_is_hidden(filepath) {
                         // if file is in a hidden path, skip it
                         continue;
                     } else if has_extension_filter {
@@ -71,11 +85,41 @@ impl ReplaceCommand {
                 if re.is_match(&filestr) {
                     let post_replace_string = re.replace_all(&filestr, replace);
                     if *inplace == true {
-                        // TODO: overwrite file
+                        // Write files in place when explicitly requested by user
+                        // ========================================================
+                        //
+                        // DANGER ZONE
+                        //
+                        // ========================================================
+                        // With directory walks this is a potentially very
+                        // dangerous code block that has the potential to cause
+                        // widespread file system mayhem.  User *must* explicitly
+                        // request that they want in place file writes to use this
+                        // functionality.
+                        // With great power comes great responsibility...
+                        let file = OpenOptions::new().write(true).create(true).open(filepath)?;
+                        let mut buffer = BufWriter::new(file);
+
+                        buffer.write_all(post_replace_string.as_bytes())?;
+                        buffer.flush()?;
+                        println!("{} updated", filepath.display());
                     } else {
-                        // TODO: create a second file path for the new file
+                        // Create a secondary file path by default
+                        let secondary_filepath = get_secondary_filepath(filepath);
+                        let file = OpenOptions::new()
+                            .write(true)
+                            .create(true)
+                            .open(&secondary_filepath)?;
+                        let mut buffer = BufWriter::new(file);
+
+                        buffer.write_all(post_replace_string.as_bytes())?;
+                        buffer.flush()?;
+                        println!(
+                            "{} updated with write to {}",
+                            filepath.display(),
+                            secondary_filepath.display()
+                        );
                     }
-                    println!("{}:\n{}", filepath.display(), post_replace_string);
                 }
             }
             Err(error) => match error.kind() {
@@ -91,7 +135,26 @@ impl ReplaceCommand {
     }
 }
 
-fn is_root_filepath(inpath: &PathBuf) -> bool {
+fn get_secondary_filepath(inpath: &Path) -> PathBuf {
+    match inpath.extension() {
+        Some(pre_ext) => {
+            let post_ext = pre_ext.to_string_lossy() + "." + SECONDARY_FILEPATH_EXTENSION;
+            return inpath.with_extension(post_ext.to_string());
+        }
+        None => {
+            return inpath.with_extension(SECONDARY_FILEPATH_EXTENSION);
+        }
+    }
+}
+
+fn has_secondary_extension(inpath: &Path) -> bool {
+    match inpath.extension() {
+        Some(ext) => return ext.to_string_lossy() == SECONDARY_FILEPATH_EXTENSION,
+        None => return false,
+    }
+}
+
+fn is_root_filepath(inpath: &Path) -> bool {
     let invalid_list = ["/", r"\"];
     let inpath_needle = inpath.to_string_lossy();
     invalid_list.contains(&inpath_needle.as_ref())
@@ -100,6 +163,50 @@ fn is_root_filepath(inpath: &PathBuf) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // ======================================
+    // get_secondary_filepath tests
+    // ======================================
+    #[test]
+    fn test_get_secondary_filepath_with_txt_extension() {
+        let testpath = PathBuf::from("test/path/bogus.txt");
+        assert_eq!(
+            get_secondary_filepath(&testpath),
+            PathBuf::from("test/path/bogus.txt.2")
+        );
+    }
+
+    #[test]
+    fn test_get_secondary_filepath_with_2_extension() {
+        let testpath = PathBuf::from("test/path/bogus.2");
+        assert_eq!(
+            get_secondary_filepath(&testpath),
+            PathBuf::from("test/path/bogus.2.2")
+        );
+    }
+
+    #[test]
+    fn test_get_secondary_filepath_without_extension() {
+        let testpath = PathBuf::from("test/path/bogus");
+        assert_eq!(
+            get_secondary_filepath(&testpath),
+            PathBuf::from("test/path/bogus.2")
+        );
+    }
+    // ======================================
+    // has_secondary_extension tests
+    // ======================================
+    #[test]
+    fn test_has_secondary_extension_with_secondary_extension() {
+        let testpath = PathBuf::from("test/path/bogus.txt.2");
+        assert!(has_secondary_extension(&testpath));
+    }
+
+    #[test]
+    fn test_has_secondary_extension_without_secondary_extension() {
+        let testpath = PathBuf::from("test/path/bogus.txt");
+        assert_eq!(has_secondary_extension(&testpath), false);
+    }
 
     // ======================================
     // is_root_filepath tests
