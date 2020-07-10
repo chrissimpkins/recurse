7c558a8ba643a580732e28162bba4dbde9eef84e
Chris Simpkins <git.simpkins@gmail.com>
2020-07-10 19:29:59 -0400

    [lib] initial draft of crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+//! # recurse
+//!
+//! ## A cross-platform recursive directory traversal file management tool
+//!
+//! ![Version](https://img.shields.io/github/v/release/chrissimpkins/recurse?sort=semver)
+//! [![GNU/Linux CI](https://github.com/chrissimpkins/recurse/workflows/GNU/Linux%20CI/badge.svg)](https://github.com/chrissimpkins/recurse/actions?query=workflow%3A%22GNU%2FLinux+CI%22)
+//! [![macOS CI](https://github.com/chrissimpkins/recurse/workflows/macOS%20CI/badge.svg)](https://github.com/chrissimpkins/recurse/actions?query=workflow%3A%22macOS+CI%22)
+//! [![Windows CI](https://github.com/chrissimpkins/recurse/workflows/Windows%20CI/badge.svg)](https://github.com/chrissimpkins/recurse/actions?query=workflow%3A%22Windows+CI%22)
+//! [![codecov](https://codecov.io/gh/chrissimpkins/recurse/branch/master/graph/badge.svg)](https://codecov.io/gh/chrissimpkins/recurse)
+//!
+//! ## About
+//!
+//! The `recurse` executable is a cross-platform, command line file management tool with *default* recursive directory traversal and regular expression pattern matching support.  It is built in Rust and tested against the stable, beta, and nightly Rust toolchains on GNU/Linux, macOS, and Windows platforms.
+//!
+//! The project is at an early stage of development and should not be considered stable for general use at the moment.
+//!
+//! Detailed documentation is available on the [repository README page](https://github.com/chrissimpkins/recurse).
+//!
+//!
+//! ## Installation
+//!
+//! ```notest
+//! $ cargo install recurse
+//! ```
+//!
+//! The `recurse` executable will be available on your system PATH after this install step.
+//!
+//! ## Usage
+//!
+//! `recurse` functionality is exposed through executable sub-commands.  Use `recurse --help` or see the [project README documentation](https://github.com/chrissimpkins/recurse) for additional details.
+//!
+//! ## Contributing
+//!
+//! Please file issue reports on the [GitHub repository issue tracker](https://github.com/chrissimpkins/recurse/issues).
+//!
+//! Source contributions under the Apache License, v2.0 are welcomed.  Please submit a pull request on the GitHub repository with your change proposal.
+//!
+//! ## License
+//!
+//! [Apache License, v2.0](https://github.com/chrissimpkins/recurse/blob/master/LICENSE.md)
+
 use std::path::PathBuf;
 
 use anyhow::Result;
