8bf65ee7ebe93ac6698001d860fcbd94ae1d138d
Chris Simpkins <git.simpkins@gmail.com>
2020-07-12 16:43:46 -0400

    [command::find] initial implementation of find sub-command

--- src/command/find.rs
+++ src/command/find.rs
@@ -25,73 +25,71 @@ impl Command for FindCommand {
             inpath,
         } = subcmd
         {
-            println!("Sub-command `find` is not implemented yet. Follow issue https://github.com/chrissimpkins/recurse/issues/4 for progress.");
+            let has_extension_filter = extension.is_some();
+            let re = Regex::new(&find)?;
+            for entry in walk(inpath, &mindepth, &maxdepth, &symlinks).filter_map(|f| f.ok()) {
+                if entry.metadata().unwrap().is_file() {
+                    let filepath = entry.path();
+                    if !hidden && path_is_hidden(filepath) {
+                        // if file is in a hidden path, skip it
+                        continue;
+                    } else if has_extension_filter {
+                        // if user requested extension filter, filter on it
+                        if path_has_extension(filepath, extension.as_ref().unwrap()) {
+                            FindCommand::print_filepath_regex_matches(&filepath, &re, &mut writer)?;
+                        }
+                    } else {
+                        FindCommand::print_filepath_regex_matches(&filepath, &re, &mut writer)?;
+                    }
+                }
+            }
             Ok(())
-        //     let has_extension_filter = extension.is_some();
-        //     let re = Regex::new(&find)?;
-        //     for entry in walk(inpath, &mindepth, &maxdepth, &symlinks).filter_map(|f| f.ok()) {
-        //         if entry.metadata().unwrap().is_file() {
-        //             let filepath = entry.path();
-        //             if !hidden && path_is_hidden(filepath) {
-        //                 // if file is in a hidden path, skip it
-        //                 continue;
-        //             } else if has_extension_filter {
-        //                 // if user requested extension filter, filter on it
-        //                 if path_has_extension(filepath, extension.as_ref().unwrap()) {
-        //                     FindCommand::print_filepath_regex_matches(&filepath, &re, &mut writer)?;
-        //                 }
-        //             } else {
-        //                 FindCommand::print_filepath_regex_matches(&filepath, &re, &mut writer)?;
-        //             }
-        //         }
-        //     }
-        //     Ok(())
         } else {
             Err(anyhow!("failure to parse find subcommand."))
         }
     }
 }
 
-// impl FindCommand {
-//     pub(crate) fn print_filepath_regex_matches(
-//         filepath: &Path,
-//         re: &Regex,
-//         writer: &mut impl Write,
-//     ) -> Result<()> {
-//         match read_to_string(&filepath) {
-//             Ok(filestr) => {
-//                 // short circuit the individual line checks if overall match does not
-//                 // indicate the presence of a match
-//                 if re.is_match(&filestr) {
-//                     // iterate through lines and print matches
-//                     let mut line_number = 0;
-//                     for line in filestr.lines() {
-//                         line_number += 1;
-//                         for mat in re.find_iter(line) {
-//                             writeln!(
-//                                 writer,
-//                                 "{} {}:{}-{} {} {} {}",
-//                                 &filepath.display(),
-//                                 &line_number.to_string().green(),
-//                                 &mat.start().to_string().green(),
-//                                 &mat.end().to_string().green(),
-//                                 "[".dimmed(),
-//                                 &mat.as_str().red().bold(),
-//                                 "]".dimmed(),
-//                             )?;
-//                         }
-//                     }
-//                 }
-//             }
-//             Err(error) => match error.kind() {
-//                 // If this was due to invalid UTF-8 conversion
-//                 // on file read, then skip the file.
-//                 // The intent is to test files with valid
-//                 // UTF-8 encodings only in this subcommand
-//                 ErrorKind::InvalidData => {}
-//                 _ => return Err(anyhow!(error)),
-//             },
-//         }
-//         Ok(())
-//     }
-// }
+impl FindCommand {
+    pub(crate) fn print_filepath_regex_matches(
+        filepath: &Path,
+        re: &Regex,
+        writer: &mut impl Write,
+    ) -> Result<()> {
+        match read_to_string(&filepath) {
+            Ok(filestr) => {
+                // short circuit the individual line checks if overall match does not
+                // indicate the presence of a match
+                if re.is_match(&filestr) {
+                    // iterate through lines and print matches
+                    let mut line_number = 0;
+                    for line in filestr.lines() {
+                        line_number += 1;
+                        for mat in re.find_iter(line) {
+                            writeln!(
+                                writer,
+                                "{} {}:{}-{} {} {} {}",
+                                &filepath.display(),
+                                &line_number.to_string().green(),
+                                &mat.start().to_string().green(),
+                                &mat.end().to_string().green(),
+                                "[".dimmed(),
+                                &mat.as_str().red().bold(),
+                                "]".dimmed(),
+                            )?;
+                        }
+                    }
+                }
+            }
+            Err(error) => match error.kind() {
+                // If this was due to invalid UTF-8 conversion
+                // on file read, then skip the file.
+                // The intent is to test files with valid
+                // UTF-8 encodings only in this subcommand
+                ErrorKind::InvalidData => {}
+                _ => return Err(anyhow!(error)),
+            },
+        }
+        Ok(())
+    }
+}
